c572478b561dc8b19261ab835a2df5549d74f01a
Ben Miller <benjivm@protonmail.com>
2020-10-14 12:20:12 -0700

    Init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex flex-col justify-center h-screen">
+        <star-rating
+            :rating="rating"
+            @update:rating="rating = $event"
+        />
+
+        <div class="mx-auto p-3">
+            Rating: {{ rating }}
+        </div>
+    </div>
+</template>
+
+<script>
+    import StarRating from './components/StarRating.vue';
+
+    export default {
+        components: {
+            StarRating
+        },
+
+        data() {
+            return {
+                rating: 0
+            };
+        }
+    };
+</script>
---
+++ src/components/StarRating.vue
@@ -0,0 +1,60 @@
+<template>
+    <div
+        @mouseleave="hoverRating = 0"
+        class="flex items-center mx-auto"
+    >
+        <button
+            v-for="star in totalStars"
+            :key="star"
+            @click="setRating(star)"
+            @mouseover="hoverRating = star"
+            class="focus:outline-none"
+        >
+            <svg
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                class="w-10 h-10 text-gray-300 -mx-1"
+                :class="{ 'text-yellow-500' : (rating >= star && !hoverRating) || hoverRating >= star }"
+            >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+            </svg>
+        </button>
+    </div>
+</template>
+
+<script>
+    import { ref, onMounted } from 'vue';
+
+    export default {
+        props: {
+            rating: {
+                type: Number,
+                required: true,
+            },
+            totalStars: {
+                type: Number,
+                required: false,
+                default: 10
+            }
+        },
+
+        emits: ['update:rating'],
+
+        setup(props, context) {
+            const hoverRating = ref(0);
+
+            function setRating(value) {
+                if (props.rating === value) {
+                    return context.emit('update:rating', 0);
+                }
+
+                context.emit('update:rating', value);
+            }
+
+            return {
+                hoverRating,
+                setRating,
+            };
+        }
+    };
+</script>
